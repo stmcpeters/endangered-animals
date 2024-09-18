551dbfd571dfc5976270f314810fd863ee31016e
stmcpeters <stmcpeters@gmail.com>
2024-09-18 12:37:06 -0700

    creates put request to edit individuals

--- server/server.js
+++ server/server.js
@@ -123,7 +123,41 @@ app.post('/api/individuals', async (req, res) => {
 
 });
 
+// // delete request for individuals
+// app.delete('/api/individuals/:id', async (req, res) => {
+//     try {
+//         const individualId = req.params.id;
+//         await db.query('DELETE FROM individuals WHERE id=$1', [individualId]);
+//         console.log(`Individual with the id: ${individualId} has been deleted`);
+//         res.status(200).end();
+//     } catch (e) {
+//         console.log(e);
+//         return res.status(400).json({ e });
 
+//     }
+// });
+
+// PUT request - Update an individual 
+app.put('/api/individuals/:id', async (req, res) =>{
+    //console.log(req.params);
+    //This will be the id that I want to find in the DB - the individual to be updated
+    const individualId = req.params.id;
+    const updatedIndividual = { individuals_nickname: req.body.individuals_nickname, sex: req.body.sex, species_id: req.body.species_id }
+    console.log("In the server from the url - the individual id", individualId);
+    console.log("In the server, from the react - the individual to be edited", updatedIndividual);
+    // UPDATE individuals SET sex = "something" WHERE id="16";
+    const query = `UPDATE individuals SET individuals_nickname=$1, sex=$2, species_id=$3 WHERE id=${individualId} RETURNING *`;
+    const values = [updatedIndividual.individuals_nickname, updatedIndividual.sex, updatedIndividual.species_id];
+    try {
+      const updated = await db.query(query, values);
+      console.log(updated.rows[0]);
+      res.send(updated.rows[0]);
+  
+    }catch(e){
+      console.log(e);
+      return res.status(400).json({e})
+    }
+  })
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
