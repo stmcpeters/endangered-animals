6a913d98b5434266aaf28df2aee08be3fc7d478c
stmcpeters <stmcpeters@gmail.com>
2024-09-22 16:12:12 -0700

    edits info displayed on sightings cards

--- client/src/components/Sightings.jsx
+++ client/src/components/Sightings.jsx
@@ -21,10 +21,11 @@ const Sightings = ({sighting, toUpdate, toDelete}) => {
                         {sighting.individual_id} <br></br>
                         {sighting.individuals_nickname}<br></br>
                         {/* splits the timestamp string at the 'T' character (separates date and time in long format) and takes date only */}
-                        {/* {sighting.sighting_date.split('T')[0]} <br></br> */}
-                        {sighting.sighting_date}<br></br>
+                        {sighting.sighting_date.split('T')[0]} <br></br>
+                        {/* {sighting.sighting_date}<br></br> */}
                         {sighting.location}<br></br>
-                        {sighting.healthy}
+                        {/* converts boolean value to string to display on card */}
+                        Healthy: {String(sighting.healthy)}<br></br>
             </Card.Title>
             <Button variant="outline-danger" onClick={()=>{onDelete(sighting)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
             <Button variant="outline-info" onClick={()=>{onUpdate(sighting)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button>
